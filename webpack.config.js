37b7678f3038eac533ba35a796530428313288e1
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2018-09-17 04:40:25 -0600

    Finished initial setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  externals: {
+    "jquery": "jQuery"
+  },
+  mode: "development"
+}
+
