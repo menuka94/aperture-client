990a7b0ab3c26e334202083b0998945b20b678db
Daniel Reynolds <danielreynolds101@gmail.com>
2020-03-16 19:45:10 -0600

    QOL and update upgrades

--- src/controller.js
+++ src/controller.js
@@ -1,10 +1,10 @@
-var view = {lat: 40.7580, lng: 286.0145};
+var view = { lat: 40.7580, lng: 286.0145 };
 var zoomLevel = 3;
 
 var setterFunctions = []; //these are added at the end of each map's html
 
 //flags which prevent other maps from moving the moused map
-var map1IsBeingMoved = false; 
+var map1IsBeingMoved = false;
 var map2IsBeingMoved = false;
 var map3IsBeingMoved = false;
 var map4IsBeingMoved = false;
@@ -14,27 +14,30 @@ var map2IsMoused = false;
 var map3IsMoused = false;
 var map4IsMoused = false;
 
+//create update event
+var updateEvent = new CustomEvent("updateMaps");
 
 
-function setGlobalZoom(_zoomLevel,mapNumber){ //this isnt used but its here anyways
-    if(checkIfAnyCanMove()){
-        initializeMap(mapNumber,true);
+/*
+function setGlobalZoom(_zoomLevel, mapNumber) { //this isnt used but its here anyways
+    if (checkIfAnyCanMove()) {
+        initializeMap(mapNumber, true);
     }
-    if(!verifyCorrectMap(mapNumber)){
+    if (!verifyCorrectMap(mapNumber)) {
         //console.log("map " + mapNumber + " wasnt the corrent map");
         return;
     }
     //console.log("setting global zoom to map " + mapNumber);
     //zoomLevel = _zoomLevel;
     //updateMaps(mapNumber);
-}
+}*/
 
-function setGlobalPosition(_view,mapNumber){
-    if(checkIfAnyCanMove()){
-        initializeMap(mapNumber,true);
+function setGlobalPosition(_view, mapNumber) {
+    if (checkIfAnyCanMove()) {
+        initializeMap(mapNumber, true);
         //console.log("map " + mapNumber + " set to true");
     }
-    if(!verifyCorrectMap(mapNumber)){
+    if (!verifyCorrectMap(mapNumber)) {
         //console.log("map1 wasnt ready for move");
         return;
     }
@@ -42,28 +45,32 @@ function setGlobalPosition(_view,mapNumber){
     view = _view;
     updateMaps(mapNumber);
 }
+function setGlobalPositionFORCE(_view, mapNumber) {
+    view = _view;
+    updateMaps(mapNumber);
+}
 
-function updateMaps(mapNumber){
-    for(var i = 1; i <= setterFunctions.length; i++){
-        if(i != mapNumber){
-            setterFunctions[i - 1](view,zoomLevel);
+function updateMaps(mapNumber) {
+    for (var i = 1; i <= setterFunctions.length; i++) {
+        if (i != mapNumber) {
+            setterFunctions[i - 1](view, zoomLevel);
             //console.log("updating map " + i);
         }
     }
 }
 
-function checkIfAnyCanMove(){ //checks if any map can start moving at the current moment
-    if(map1IsBeingMoved || map2IsBeingMoved || map3IsBeingMoved || map4IsBeingMoved){
+function checkIfAnyCanMove() { //checks if any map can start moving at the current moment
+    if (map1IsBeingMoved || map2IsBeingMoved || map3IsBeingMoved || map4IsBeingMoved) {
         return false;
     }
-    else{
+    else {
         return true;
     }
 }
 
-function initializeMap(mapNumber,TorF){ //sets mapNumber to True or False, this also checks if the map is being moused as verification
+function initializeMap(mapNumber, TorF) { //sets mapNumber to True or False, this also checks if the map is being moused as verification
     //console.log("setting map " + mapNumber + " to true");
-    switch(mapNumber){
+    switch (mapNumber) {
         case 1:
             map1IsBeingMoved = TorF && map1IsMoused;
             break;
@@ -82,9 +89,9 @@ function initializeMap(mapNumber,TorF){ //sets mapNumber to True or False, this 
 }
 
 
-function verifyCorrectMap(mapNumber){
-    if(map1IsBeingMoved || map2IsBeingMoved || map3IsBeingMoved || map4IsBeingMoved){ //check if any map is being moved currently
-        switch(mapNumber){ //checks if the map that is calling the function is the one being moved, returns true if it is, false if it isnt
+function verifyCorrectMap(mapNumber) {
+    if (map1IsBeingMoved || map2IsBeingMoved || map3IsBeingMoved || map4IsBeingMoved) { //check if any map is being moved currently
+        switch (mapNumber) { //checks if the map that is calling the function is the one being moved, returns true if it is, false if it isnt
             case 1:
                 return map1IsBeingMoved;
             case 2:
@@ -97,20 +104,20 @@ function verifyCorrectMap(mapNumber){
                 return false;
         }
     }
-    else{
+    else {
         return false;
     }
 }
 
 //mouse control functions
-document.addEventListener('mousedown', function(event) { //since these two being called means the mouse is not on any map, set all to false
+document.addEventListener('mousedown', function (event) { //since these two being called means the mouse is not on any map, set all to false
     mouseUp();
 });
-document.addEventListener('mouseup', function(event) {
+document.addEventListener('mouseup', function (event) {
     mouseUp();
 });
 
-function mouseUp(){ //called whenever a mouse comes up or isnt over a map when clicked
+function mouseUp() { //called whenever a mouse comes up or isnt over a map when clicked
     map1IsMoused = false;
     map2IsMoused = false;
     map3IsMoused = false;
@@ -119,10 +126,11 @@ function mouseUp(){ //called whenever a mouse comes up or isnt over a map when c
     map2IsBeingMoved = false;
     map3IsBeingMoved = false;
     map4IsBeingMoved = false;
+    updateAll();
 }
 
-function mouseDown(mapNumber){ //called from iframe of map, allows map to be moved
-    switch(mapNumber){
+function mouseDown(mapNumber) { //called from iframe of map, allows map to be moved
+    switch (mapNumber) {
         case 1:
             map1IsMoused = true;
             break;
@@ -137,5 +145,18 @@ function mouseDown(mapNumber){ //called from iframe of map, allows map to be mov
             break;
         default:
             return;
+    }
+}
+
+function updateAll() {
+    window.dispatchEvent(updateEvent);
+}
+
+var loadCount = 0;
+function loaded(){
+    loadCount++;
+    if(loadCount >= 3){
+        console.log("Very loaded");
+        document.getElementById("loadCover").style.display = "none";
     }
 }
