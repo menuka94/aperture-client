90859beb0e5bdd209e250b9db6c6b867a34aaf61
Daniel Reynolds <danielreynolds101@gmail.com>
2020-04-09 16:52:46 -0600

    Optimized querying ALOT and added some cool new stuff

--- src/Iframe/Dependencies/getInfrastructure.js
+++ src/Iframe/Dependencies/getInfrastructure.js
@@ -15,7 +15,8 @@ let currentLayers = [];
 function updateObjects(queryListOrig,bounds,markerCluster,map,cleanUpMap){ //gets the objects within the current viewport
     let sw = bounds.getSouthWest().wrap();
     let ne = bounds.getNorthEast().wrap();
-    let queryList= queryListOrig.slice() //clone querylist
+    let queryList = queryListOrig.slice(); //clone querylist
+
     let bBounds = {
         north: ne.lat,
         south: sw.lat,
@@ -28,9 +29,8 @@ function updateObjects(queryListOrig,bounds,markerCluster,map,cleanUpMap){ //get
             queryList.splice(i,1);
         }
     }*/
-    let queryFString = createQuery(queryList,boundsString);
-    let fQuery = '?data=[out:json][timeout:15];(' + queryFString + ');out body geom;';
-    queryObjectsFromServer(map,fQuery,markerCluster,cleanUpMap);
+    let queryURL = queryDefault(queryList,boundsString);
+    queryObjectsFromServer(queryURL,map,markerCluster,cleanUpMap,bBounds);
     updateObjectsPan(bBounds,boundsString,queryList,map,markerCluster);
 }
 
@@ -48,10 +48,53 @@ function createQuery(queryList,boundsString){
     return queryFString;
 }
 
-function queryObjectsFromServer(map,fQuery,markerCluster,cleanUpMap){
-    $.getJSON('https://overpass.kumi.systems/api/interpreter' + fQuery, function(osmDataAsJson) {
-        drawObjectsToMap(map,osmtogeojson(osmDataAsJson),markerCluster,cleanUpMap);
-    });
+let currentQueries = [];
+let currentBounds;
+function queryObjectsFromServer(queryURL,map,markerCluster,cleanUpMap,bounds){
+    if(!withinBounds(bounds)){
+        for(let i = 0; i < currentQueries.length; i++){
+            if(queryNeedsCancelling(currentQueries[i],bounds)){
+                currentQueries.splice(i,1);
+            }
+        }
+        currentBounds = bounds;
+        let query = $.getJSON(queryURL, function(osmDataAsJson) {
+            if(map.getZoom() >= MINRENDERZOOM){
+                queryAlertText.parentElement.style.display = "none";
+            }
+            drawObjectsToMap(map,osmtogeojson(osmDataAsJson),markerCluster,cleanUpMap);
+        });
+        currentQueries.push({query:query,bounds:bounds});
+    }
+}
+
+function withinBounds(boundsToTest){
+    if(currentBounds == null){
+        return false;
+    }
+    //console.log(currentBounds.north > boundsToTest.north);
+    if(currentBounds.north > boundsToTest.north && currentBounds.south < boundsToTest.south && currentBounds.west < boundsToTest.west && currentBounds.east > boundsToTest.east){
+        return true;
+    }
+    return false;
+}
+
+function queryNeedsCancelling(queryObj,boundsToCheckAgainst){
+    if(queryObj.bounds.east < boundsToCheckAgainst.west || queryObj.bounds.west > boundsToCheckAgainst.east || queryObj.bounds.south > boundsToCheckAgainst.north || queryObj.bounds.north < boundsToCheckAgainst.south){
+        queryObj.query.abort();
+        return true;
+    }
+    return false;
+}
+
+function queryDefault(queryList,boundsString){
+    let queryFString = createQuery(queryList,boundsString);
+    let fQuery = '?data=[out:json][timeout:15];(' + queryFString + ');out body geom;';
+    return 'https://overpass.kumi.systems/api/interpreter' + fQuery;
+}
+
+function queryNaturalGas(){
+    //https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/Natural_Gas_Liquid_Pipelines/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-122.554%2C36.544%2C-119.940%2C36.930&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json
 }
 
 function drawObjectsToMap(map,dataToDraw,markerCluster,cleanUpMap){
@@ -93,6 +136,10 @@ function drawObjectsToMap(map,dataToDraw,markerCluster,cleanUpMap){
             latlng = latlng.reverse();
             let iconName = parseIconNameFromContext(feature);
             addIconToMap(getAttribute(iconName,ATTRIBUTE.icon),markerCluster,latlng,map,iconName);
+            layer.bindPopup(iconName);
+            layer.on('click', function(e) {
+                map.flyToBounds(layer.getBounds(),FLYTOOPTIONS);
+            });
         },
         pointToLayer: function() {
             return L.marker([0,0],{
@@ -106,6 +153,8 @@ function drawObjectsToMap(map,dataToDraw,markerCluster,cleanUpMap){
 
 function cleanupCurrentMap(map,markerCluster){
     currentLayers = [];
+    currentBounds = null;
+    currentQueries = [];
     map.eachLayer(function(layer){
         if(layer.feature != null){
             if(layer.feature.properties.type == 'node' || layer.feature.properties.type == 'way' || layer.feature.properties.type == 'relation'){
@@ -120,15 +169,15 @@ function cleanupCurrentMap(map,markerCluster){
 function updateObjectsPan(origBounds,origBoundsString,queryList,map,markerCluster){ //this function updates the objects around the current viewport, since users 
                                          //generally pan around when looking at the map, therefore there's less loading time seen by the user time.
     let newBounds = {
-        north: origBounds.north + (origBounds.north - origBounds.south) * 2,
-        south: origBounds.south - (origBounds.north - origBounds.south) * 2,
-        east: origBounds.east + (origBounds.east - origBounds.west) * 2,
-        west: origBounds.west - (origBounds.east - origBounds.west) * 2
+        north: origBounds.north + (origBounds.north - origBounds.south),
+        south: origBounds.south - (origBounds.north - origBounds.south),
+        east: origBounds.east + (origBounds.east - origBounds.west),
+        west: origBounds.west - (origBounds.east - origBounds.west)
     }
     let newBoundsString = newBounds.south + ',' + newBounds.west + ',' + newBounds.north + ',' + newBounds.east;
     let queryFString = createQuery(queryList,newBoundsString);
-    let fQuery = '?data=[out:json][timeout:15];(' + queryFString + ')->.a;(.a;-node(' + origBoundsString + ');)->.a;(.a;-way(' + origBoundsString + ');)->.a;(.a;-relation(' + origBoundsString + '););out body geom;';
-    queryObjectsFromServer(map,fQuery,markerCluster);
+    let queryURL = 'https://overpass.kumi.systems/api/interpreter?data=[out:json][timeout:15];(' + queryFString + ')->.a;(.a;-node(' + origBoundsString + ');)->.a;(.a;-way(' + origBoundsString + ');)->.a;(.a;-relation(' + origBoundsString + '););out body geom;';
+    queryObjectsFromServer(queryURL,map,markerCluster,false,newBounds);
 }
 
 function removeFromMap(idToRemove,layerToRemoveFrom,mapToRemoveFrom){
@@ -150,7 +199,7 @@ function removeFromMap(idToRemove,layerToRemoveFrom,mapToRemoveFrom){
 }
 
 //icon getters ------------------------------------------------
-var commonTagNames = ["amenity","man_made","waterway","landuse"];
+var commonTagNames = ["amenity","man_made","waterway","landuse","water"];
 var blacklist = ["yes","amenity"];
 
 function parseIconNameFromContext(feature){
@@ -240,7 +289,7 @@ function getAttribute(option,attribute) {
                 iconUrl: "../../../images/water_works.png",
                 iconSize: [25, 25]
             });
-            color = "#00FFFF";
+            color = "black";
             break;
         case "wastewater_plant":
             icon = new L.Icon({
@@ -249,6 +298,42 @@ function getAttribute(option,attribute) {
             });
             color = "#FF00FF";
             break;
+        case "pipeline":
+            icon = new L.Icon({
+                iconUrl: "../../../images/sewage.png",
+                iconSize: [25, 25]
+            });
+            color = "#FFFF00";
+            break;
+        case "reservoir":
+            icon = new L.Icon({
+                iconUrl: "../../../images/water_works.png",
+                iconSize: [25, 25]
+            });
+            color = "#00FFFF";
+            break;
+        case "canal":
+            icon = new L.Icon({
+                iconUrl: "../../../images/water_works.png",
+                iconSize: [25, 25]
+            });
+            color = "teal";
+            break;
+        case "river":
+            icon = new L.Icon({
+                iconUrl: "../../../images/water_works.png",
+                iconSize: [25, 25]
+            });
+            color = "#00008B";
+            break;
+        case "basin":
+            icon = new L.Icon({
+                iconUrl: "../../../images/water_works.png",
+                iconSize: [25, 25]
+            });
+            color = "green";
+            break;
+        
     }
     if(attribute == ATTRIBUTE.icon){
         return icon;
