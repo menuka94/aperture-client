334a8fb32968150ef094d8c281eb92a54e7b1b9d
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2020-04-13 14:54:25 -0600

    added function to calculate smallest bounding geohash and updated query to use it

--- src/Iframe/Dependencies/Sketch_Visualization/sketch_visualizer.js
+++ src/Iframe/Dependencies/Sketch_Visualization/sketch_visualizer.js
@@ -49,8 +49,22 @@ Sketch_Visualizer = {
         return [r, g, b, alpha];
     },
 
+    getBoundingGeohash: function(bounds) {
+        const b1 = encode_geohash(bounds._northEast.lat, bounds._northEast.lng);
+        const b2 = encode_geohash(bounds._southWest.lat, bounds._southWest.lng);
+        let boundingGeo = "";
+        for(let i = 0; i < b1.length; i++){
+            if (b1.charAt(i) === b2.charAt(i)){
+                boundingGeo += b1.charAt(i)
+            } else { break; }
+        }
+        console.log(boundingGeo)
+        return boundingGeo
+    },
+
     queryTime: function(startTime, endTime, ctx, map) {
-        const stream = this._grpcQuerier.getStreamForQuery("noaa_2015_jan", ["",], startTime, endTime);
+        const geohashList = [this.getBoundingGeohash(map.getBounds()),];
+        const stream = this._grpcQuerier.getStreamForQuery("noaa_2015_jan", geohashList, startTime, endTime);
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         stream.on('data', function (response) {
             for (const strand of response.getStrandsList()) {
