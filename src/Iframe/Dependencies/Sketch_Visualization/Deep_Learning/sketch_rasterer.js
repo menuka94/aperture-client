26024256ca37269444c22d45160441f74dd47a2f
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2020-04-23 14:12:15 -0600

    able to successfully run model on toy data

--- src/Iframe/Dependencies/Sketch_Visualization/Deep_Learning/sketch_rasterer.js
+++ src/Iframe/Dependencies/Sketch_Visualization/Deep_Learning/sketch_rasterer.js
@@ -1,13 +1,27 @@
 Sketch_Rasterer = {
     initialize: async function () {
-        const session = new onnx.InferenceSession();
+        const session = new onnx.InferenceSession({});
         const url = "Dependencies/Sketch_Visualization/Deep_Learning/viz_net.onnx";
-        await session.loadModel(url);
-       // const model =  await tf.loadLayersModel("Sketch_Visualization/Deep_Learning/saved_model.pb");
+        await session.loadModel(url).then(() => {
+            const input = this._getInput();
+            const query = this._getQuery();
 
-        //const runner = await webdnn.load('graph_descriptor');
+            session.run([input, query]).then(output => {
+                let outputTensor = output.values().next().value;
+                console.log(`model output tensor: ${outputTensor.data}.`);
+            });
+        });
     },
 
+    _getInput: function() {
+        const x = new Float32Array(10 * 400 * 850).fill(0.5);
+        return new onnx.Tensor(x, "float32", [1, 10, 1, 400, 850]);
+    },
+
+    _getQuery: function() {
+        const q = new Float32Array(10 * 13).fill(0.5);
+        return new onnx.Tensor(q, "float32", [10, 13]);
+    }
 };
 
 
