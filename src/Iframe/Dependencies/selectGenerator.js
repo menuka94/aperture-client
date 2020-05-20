bf158dbca9f3e688110c78722d65c0f5a9862864
Daniel <danielreynolds101@gmail.com>
2020-05-20 13:00:54 -0600

    Color coded the infrastructure

--- src/Iframe/Dependencies/selectGenerator.js
+++ src/Iframe/Dependencies/selectGenerator.js
@@ -21,7 +21,8 @@ let Generator = {
         }
         for(element in elementsJson){
             let checked = elementsJson[element]['defaultRender'] ? 'checked' : '';
-            selectContainer.innerHTML+='<input type="checkbox" id="' + element + '" onchange="' + callFunc.name + '(this)" ' + checked + '><label for="' + element + '">' + Util.capitalizeString(Util.underScoreToSpace(element)) + '</label><br>'
+            let color = colorCode && elementsJson[element]['color'] ? 'style="border-bottom:3px solid ' + elementsJson[element]['color'] + ';"' : '';
+            selectContainer.innerHTML+='<div style="margin-top:3px;margin-bottom:3px"><input type="checkbox" id="' + element + '" onchange="' + callFunc.name + '(this)" ' + checked + '><label for="' + element + '" ' + color + '>' + Util.capitalizeString(Util.underScoreToSpace(element)) + '</label></div>'
         }
     }
 }
